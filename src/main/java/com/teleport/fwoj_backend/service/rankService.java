d66e1b9fa9b8dd4fcfedc344660fd426e94a0dcd
Teleport <58069438+teleport-10032@users.noreply.github.com>
2021-02-13 08:23:36 +0800

    ud

--- src/main/java/com/teleport/fwoj_backend/service/rankService.java
+++
@@ -1,8 +0,0 @@
-package com.teleport.fwoj_backend.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface rankService{
-    String  getRank(int page, int pre) throws JsonProcessingException;
-    String getRankChartData() throws JsonProcessingException;
-}
