4238ffa8ceb11e0b5f0b1c54f487766cc17577ad
Teleport <58069438+teleport-10032@users.noreply.github.com>
2021-03-18 23:11:41 +0800

    Update FwojBackendApplicationTests.java

--- src/test/java/com/teleport/fwoj_backend/FwojBackendApplicationTests.java
+++ src/test/java/com/teleport/fwoj_backend/FwojBackendApplicationTests.java
@@ -1,27 +1,14 @@
 package com.teleport.fwoj_backend;
-
-import com.teleport.fwoj_backend.mapper.annMapper;
-import com.teleport.fwoj_backend.mapper.contestMapper;
-import com.teleport.fwoj_backend.mapper.problemMapper;
-import com.teleport.fwoj_backend.mapper.userMapper;
-import com.teleport.fwoj_backend.pojo.user;
-import com.teleport.fwoj_backend.service.problemService;
-import com.teleport.fwoj_backend.service.userService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 @SpringBootTest
 class FwojBackendApplicationTests {
 
-    @Autowired
-    userMapper userMapperO;
-
     @Test
     void contextLoads() {
-        System.out.println(userMapperO.emailExist("12ijoijioasd"));
+
     }
 
 }
